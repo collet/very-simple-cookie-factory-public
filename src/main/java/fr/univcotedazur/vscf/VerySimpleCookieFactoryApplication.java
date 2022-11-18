8c0d8cee76dd46c94bc1a1cf166c752252fd18dc
Philippe Collet <Philippe.Collet@unice.fr>
2022-11-18 11:12:22 +0100

    build: Spring init from intializr + UTF8 setup

---
+++ src/main/java/fr/univcotedazur/vscf/VerySimpleCookieFactoryApplication.java
@@ -0,0 +1,13 @@
+package fr.univcotedazur.vscf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerySimpleCookieFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerySimpleCookieFactoryApplication.class, args);
+	}
+
+}
