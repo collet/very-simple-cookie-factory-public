a7321aafe24c9dcbe6f50e183377db0816d2a67c
Philippe Collet <Philippe.Collet@unice.fr>
2022-11-20 22:54:20 +0100

    logs on components with aspects (active on tests)

--- src/main/java/fr/univcotedazur/vscf/VerySimpleCookieFactoryApplication.java
+++ src/main/java/fr/univcotedazur/vscf/VerySimpleCookieFactoryApplication.java
@@ -2,8 +2,10 @@ package fr.univcotedazur.vscf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class VerySimpleCookieFactoryApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/fr/univcotedazur/vscf/aspects/ComponentLogger.java
@@ -0,0 +1,34 @@
+package fr.univcotedazur.vscf.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ComponentLogger {
+
+        private static final Logger LOG = LoggerFactory.getLogger(ComponentLogger.class);
+        private static final String PREFIX = "VSCF:Component:";
+
+        @Pointcut("execution(public * fr.univcotedazur.vscf.components..*(..))")
+        private void allControllerMethods() {} // This enables to attach the pointcut to a method name we can reuse below
+
+        @Before("allControllerMethods()")
+        public void logMethodNameAndParametersAtEntry(JoinPoint joinPoint) {
+            LOG.info(PREFIX + joinPoint.getThis() + ":Called {}", joinPoint.getSignature().getName() + " " + joinPoint.getArgs());
+        }
+
+        @AfterReturning(pointcut = "allControllerMethods()", returning = "resultVal")
+        public void logMethodReturningProperly(JoinPoint joinPoint, Object resultVal) {
+            LOG.info(PREFIX + joinPoint.getThis() + ":Returned {}", joinPoint.getSignature().getName() + " with value " + resultVal);
+        }
+
+        @AfterThrowing(pointcut = "allControllerMethods()", throwing = "exception")
+        public void logMethodException(JoinPoint joinPoint, Exception exception) {
+            LOG.warn(PREFIX + joinPoint.getThis() + ":Exception from {}", joinPoint.getSignature().getName() + " with exception " + exception);
+        }
+
+}
