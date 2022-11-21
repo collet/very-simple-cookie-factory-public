faf56eb962c463355efe119d35092e20fdf53135
Philippe Collet <Philippe.Collet@unice.fr>
2022-11-21 11:34:59 +0100

    refactor: renaming AOP setup as it intercepts components and not controllers

--- src/main/java/fr/univcotedazur/vscf/aspects/ComponentLogger.java
+++ src/main/java/fr/univcotedazur/vscf/aspects/ComponentLogger.java
@@ -14,19 +14,19 @@ public class ComponentLogger {
         private static final String PREFIX = "VSCF:Component:";
 
         @Pointcut("execution(public * fr.univcotedazur.vscf.components..*(..))")
-        private void allControllerMethods() {} // This enables to attach the pointcut to a method name we can reuse below
+        private void allComponentsMethods() {} // This enables to attach the pointcut to a method name we can reuse below
 
-        @Before("allControllerMethods()")
+        @Before("allComponentsMethods()")
         public void logMethodNameAndParametersAtEntry(JoinPoint joinPoint) {
             LOG.info(PREFIX + joinPoint.getThis() + ":Called {}", joinPoint.getSignature().getName() + " " + joinPoint.getArgs());
         }
 
-        @AfterReturning(pointcut = "allControllerMethods()", returning = "resultVal")
+        @AfterReturning(pointcut = "allComponentsMethods()", returning = "resultVal")
         public void logMethodReturningProperly(JoinPoint joinPoint, Object resultVal) {
             LOG.info(PREFIX + joinPoint.getThis() + ":Returned {}", joinPoint.getSignature().getName() + " with value " + resultVal);
         }
 
-        @AfterThrowing(pointcut = "allControllerMethods()", throwing = "exception")
+        @AfterThrowing(pointcut = "allComponentsMethods()", throwing = "exception")
         public void logMethodException(JoinPoint joinPoint, Exception exception) {
             LOG.warn(PREFIX + joinPoint.getThis() + ":Exception from {}", joinPoint.getSignature().getName() + " with exception " + exception);
         }
